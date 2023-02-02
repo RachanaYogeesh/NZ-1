4499a46e96fc4c2bb85711ee6971e4b2404a1c48
rachanayogeesh <yrachana03@gmail.com>
2023-02-02 14:04:13 +0530

    Section3

---
+++ NZWalks.API/Repositories/RegionRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class RegionRepository : IRegionRepository
+    {
+        private readonly NZWalksDbContext nZWalksDbContext;
+        public RegionRepository(NZWalksDbContext nzwalksDbContext)
+        {
+            this.nZWalksDbContext= nzwalksDbContext;
+        }
+        public async Task<IEnumerable<Region>> GetAllAsync()
+        {
+            return await nZWalksDbContext.Regions.ToListAsync();
+        }
+    }
+}
---
+++ NZWalks.APITests/Controllers/RegionsControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NZWalks.API.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NZWalks.API.Controllers.Tests
+{
+    [TestClass()]
+    public class RegionsControllerTests
+    {
+        [TestMethod()]
+        public void GetAllRegionsTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
